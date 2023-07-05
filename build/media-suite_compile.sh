60cb6f1de1445882fd1ecbcb28d407802dffc206
gaivai <138295600+gaivai@users.noreply.github.com>
2023-07-05 20:10:31 +0800

    Update media-suite_compile.sh

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1800,11 +1800,12 @@ if  { ! mpv_disabled vapoursynth || enabled vapoursynth; }; then
         do_checkIfExist
     fi
 
-    _vsver=61
+    _vsver=57.A8
+    do_addOption --enable-tvai
     _check=(lib{vapoursynth,vsscript}.a vapoursynth{,-script}.pc vapoursynth/{VS{Helper,Script},VapourSynth}.h)
     if pc_exists "vapoursynth = $_vsver" && files_exist "${_check[@]}"; then
         do_print_status "vapoursynth R$_vsver" "$green" "Up-to-date"
-    elif do_wget "https://github.com/vapoursynth/vapoursynth/releases/download/R$_vsver/VapourSynth${bits%bit}-Portable-R$_vsver.7z"; then
+    elif do_wget "https://github.com/AmusementClub/vapoursynth-classic/releases/download/R$_vsver/release-x${bits%bit}.zip"; then
         do_uninstall {vapoursynth,vsscript}.lib include/vapoursynth "${_check[@]}"
         do_install sdk/include/*.h include/vapoursynth/
 
